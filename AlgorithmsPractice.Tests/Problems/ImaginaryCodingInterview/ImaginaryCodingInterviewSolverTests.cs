f9d2d3d1f64fa4d5ceb97f51aa2c435b87443758
Shad Ahm <arsh90@gmail.com>
2021-11-18 22:15:30 +0800

    Easy question

---
+++ AlgorithmsPractice.Tests/Problems/ImaginaryCodingInterview/ImaginaryCodingInterviewSolverTests.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+using AlgorithmsPractice.Problems.ImaginaryCodingInterview;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsPractice.Problems.ImaginaryCodingInterview.Tests
+{
+    public class ImaginaryCodingInterviewSolverTests
+    {
+        ImaginaryCodingInterviewSolver _imaginaryCodingInterviewSolver;
+
+        public ImaginaryCodingInterviewSolverTests()
+        {
+            _imaginaryCodingInterviewSolver = new ImaginaryCodingInterviewSolver();
+        }
+
+        [Theory()]
+        [InlineData(new int[] { 5, 5, 10, 10, 15, 15, 20, 20 }, 120, "qualified")]
+        [InlineData(new int[] { 2, 3, 8, 6, 5, 12, 10, 18 }, 120, "qualified")]
+        [InlineData(new int[] { 2, 3, 8, 6, 5, 12, 10, 18 }, 64, "qualified")]
+        [InlineData(new int[] { 5, 5, 10, 10, 25, 15, 20, 20 }, 120, "disqualified")]
+        [InlineData(new int[] { 5, 5, 10, 10, 15, 15, 20 }, 120, "disqualified")]
+        [InlineData(new int[] { 5, 5, 10, 10, 15, 15, 20, 20 }, 130, "disqualified")]
+        [InlineData(new int[] { 5, 5, 10, 10, 15, 20, 50 }, 160, "disqualified")]
+        [InlineData(new int[] { 5, 5, 10, 10, 15, 15, 40 }, 120, "disqualified")]
+        [InlineData(new int[] { 10, 10, 15, 15, 20, 20 }, 150, "disqualified")]
+        [InlineData(new int[] { 5, 5, 10, 20, 15, 15, 20, 20 }, 140, "disqualified")]
+        public void SolveTest(int[] arr, int tot, string expected)
+        {
+            var actual = _imaginaryCodingInterviewSolver.Solve(arr, tot);
+
+            Assert.Equal(actual, expected);
+        }
+    }
+}
---
+++ AlgorithmsPractice/Problems/ImaginaryCodingInterview/ImaginaryCodingInterviewSolver.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsPractice.Problems.ImaginaryCodingInterview;
+
+public class ImaginaryCodingInterviewSolver
+{
+    private const int MaxAllowedMinutes = 120;
+    private const string Disqualified = "disqualified";
+    private const string Qualified = "qualified";
+    private readonly QuestionDifficulty[] QuestionDifficulties =
+    {
+        QuestionDifficulty.VeryEasy,
+        QuestionDifficulty.VeryEasy,
+        QuestionDifficulty.Easy,
+        QuestionDifficulty.Easy,
+        QuestionDifficulty.Medium,
+        QuestionDifficulty.Medium,
+        QuestionDifficulty.Hard,
+        QuestionDifficulty.Hard
+    };
+
+    public string Solve(int[] timesTaken, int totalInterviewMinutes)
+    {
+        if(totalInterviewMinutes > MaxAllowedMinutes)
+            return Disqualified;
+
+        if (timesTaken.Length < QuestionDifficulties.Length)
+            return Disqualified;
+
+        for (int i = 0; i < timesTaken.Length; i++)
+        {
+            int maxAllowedMins = MaxAllowedTimeForDifficulty(QuestionDifficulties[i]);
+
+            if(timesTaken[i] > maxAllowedMins)
+                return Disqualified;
+        }
+
+        return Qualified; 
+    }
+
+    private int MaxAllowedTimeForDifficulty(QuestionDifficulty questionDifficulty)
+    {
+        switch (questionDifficulty)
+        {
+            case QuestionDifficulty.VeryEasy:
+                return 5;
+            case QuestionDifficulty.Easy:
+                return 10;
+            case QuestionDifficulty.Medium:
+                return 15;
+            default:
+                return 20;
+        }
+    }
+
+    private enum QuestionDifficulty
+    {
+        VeryEasy,
+        Easy,
+        Medium,
+        Hard
+    }
+}
