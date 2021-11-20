c78340fbddc1be0d5d12f86483eb66b75b8cecb8
Shad Ahm <arsh90@gmail.com>
2021-11-20 17:32:10 +0800

    Two-Sum solver

---
+++ AlgorithmsPractice.Tests/Problems/TwoSum/TwoSumSolverTests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using AlgorithmsPractice.Problems.TwoSum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsPractice.Problems.TwoSum.Tests
+{
+    public class TwoSumSolverTests
+    {
+        private readonly TwoSumSolver _solver;
+
+        public TwoSumSolverTests()
+        {
+            _solver = new TwoSumSolver();
+        }
+
+        [Fact()]
+        public void SolveTest()
+        {
+            var nums = new int[] { 2, 7, 11, 15 };
+            var target = 9;
+
+            var actual = _solver.Solve(nums, target);
+
+            Assert.Equal(new int[] { 0, 1 }, actual);
+        }
+    }
+}
---
+++ AlgorithmsPractice/Problems/TwoSum/TwoSumSolver.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsPractice.Problems.TwoSum;
+
+//https://leetcode.com/problems/two-sum/
+public class TwoSumSolver
+{
+    public int[] Solve(int[] nums, int target)
+    {
+        if (nums.Length == 1)
+            return new int[] { 0 };
+
+        return Calculate(nums, target, 0, 1, new HashSet<int>());
+    }
+
+    private int[] Calculate(int[] nums, int target, int index, int index2, HashSet<int> deadPaths)
+    {
+        if (index != index2)
+        {
+            if (nums[index] + nums[index2] == target)
+                return new int[2] { index, index2 };
+
+            if (nums[index] >= target)
+            {
+                deadPaths.Add(index);
+            }
+
+            index2++;
+
+            if (index2 == nums.Length - 1)
+            {
+                index2 = 0;
+                index++;
+            }
+        }
+        else
+        {
+            index2++;
+        }
+
+        return Calculate(nums, target, index, index2, deadPaths);
+    }
+}
